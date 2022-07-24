80127ac6ed01e31eef7694d918cac157bdf5f58a
fuboat <fuboat@outlook.com>
2022-07-24 12:06:52 +0000

    add duckdb and sqlite exp support

--- graphdraw/avg_graph_example.py
+++ graphdraw/avg_graph_example.py
@@ -1,36 +1,78 @@
+from os import path
 from matplotlib import pyplot as plt
 from examples.llvmCoverage import *
 from utils.utils import *
+import numpy
+
+# dbname = 'mariadb'
+# fix_index = 10
+# tool_names = [
+#     ('squirrel', 'r', '--', 'SQUIRREL'),
+#     ('sqlancer', 'g', '-.', 'SQLancer'),
+#     ('sqlmix',   'b', ':', 'GRIFFIN'),
+#     # ('sqlsmith', 'purple', '-', 'SQLsmith')
+# ]
+
+# dbname = 'sqlite'
+# fix_index = 10
+# tool_names = [
+#     ('squirrel', 'r', '--', 'SQUIRREL'),
+#     ('sqlancer', 'g', '-.', 'SQLancer'),
+#     ('sqlmix',   'b', ':', 'GRIFFIN'),
+#     ('sqlsmith', 'purple', '-', 'SQLsmith')
+# ]
+
+# dbname = 'postgres'
+# fix_index = 12
+# tool_names = [
+#     ('squirrel', 'r', '--', 'SQUIRREL'),
+#     ('sqlancer', 'g', '-.', 'SQLancer'),
+#     ('sqlmix',   'b', ':', 'GRIFFIN'),
+#     ('sqlsmith', 'purple', '-', 'SQLsmith')
+# ]
+
+dbname = 'duckdb'
+fix_index = 266
+tool_names = [
+    # ('squirrel', 'r', '--', 'SQUIRREL'),
+    ('sqlancer', 'g', '-.', 'SQLancer'),
+    ('sqlmix',   'b', ':', 'GRIFFIN'),
+    ('sqlsmith', 'purple', '-', 'SQLsmith')
+]
 
 if __name__ == '__main__':
 
-    fig: Figure = plt.figure(figsize=(9,7))
+    fig: Figure = plt.figure(figsize=(4,4))
     ax: Axes = fig.add_subplot(1, 1, 1)
     ax.set_xscale('log')
 
-    tool_names = [('squirrel', 'r', '--', 'SQUIRREL'), 
-                  ('sqlancer', 'g', '-.', 'SQLancer'), 
-                  ('sqlmix',   'b', ':', 'GRIFFIN')]
-
     for (tool_name, c, ls, label_name) in tool_names:
 
-        p = PointConvertForFillBetween()
+        p = PointConvertForFillBetweenV2()
 
         # squirrel
 
         for i in range(1, 6):
-            (x, y) = coverage_log_file_to_x_time_y_branch_data('data/%s/log%s' % (tool_name, i))
+
+            file_name = '/root/jingzhou_workspace/exp-data/Griffin-ASE2022/unformat/%s_%s-%s.llvm.log' % (tool_name, dbname, i)
+
+            if not path.isfile(file_name):
+                print("file not found: %s" % file_name)
+                continue
+
+            (x, y) = coverage_log_file_to_x_time_y_branch_data(file_name)
 
             if len(x) == 0:
                 continue
 
             p.add_plot(x, y)
 
-        ax.plot(p.x, p.y_avg, label=label_name, ls=ls, c=c)
-        ax.fill_between(p.x, p.y_min, p.y_max, alpha=0.2, facecolor=c)
-        ax.set_xlim(left=50, right=86400)
+        p.format_data()
+        ax.plot(list(numpy.array(p.x) - fix_index), p.y_median, label=label_name, ls=ls, c=c)
+        ax.fill_between(list(numpy.array(p.x) - fix_index), p.y_95confidence_interval_min, p.y_95confidence_interval_max, alpha=0.2, facecolor=c)
+        ax.set_xlim(left=1, right=43200)
     
     fig.legend()
 
-    fig.savefig("show.pdf", bbox_inches='tight')
-    fig.savefig("show.png")
+    fig.savefig("show-%s.pdf" % dbname, bbox_inches='tight')
+    fig.savefig("show-%s.png" % dbname)
--- graphdraw/utils/utils.py
+++ graphdraw/utils/utils.py
@@ -3,6 +3,7 @@ import re
 import shelve
 from contextlib import closing
 import pprint
+from statistics import median
 from turtle import color
 from typing import List, Tuple
 from matplotlib.axes import Axes, SubplotBase
@@ -10,91 +11,53 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.ticker import FuncFormatter
 import matplotlib
+import numpy as np
+import scipy.stats as st
 
 X = Y1 = Y2 = Y = Y_MIN = Y_MAX = List
 
 def point_convert_for_fill_between_get_avg(x1: List, y1: List, x2: List, y2: List, y1_weight: int, y2_weight: int) -> Tuple [X, Y]:
-
-    x3 = []
+    
+    (x3, y31, y32) = point_convert_for_fill_between_get_both(x1, y1, x2, y2)
     y3 = []
 
-    i1 = 0
-    i2 = 0
+    assert len(y31) == len(y32)
 
-    def avg(y1, y2):
-        return ((y1 * y1_weight) + (y2 * y2_weight)) / (y1_weight + y2_weight)
+    for i in range(len(y31)):
+        y3.append((y31[i] * y1_weight + y32[i] * y2_weight) / (y1_weight + y2_weight))
 
-    while i1 < len(x1) and i2 < len(x2):
-
-        if x1[i1] == x2[i2]:
-            x3.append(x1[i1])
-            y3.append(avg(y1[i1], y2[i2]))
-            i1 += 1
-            i2 += 1
-
-        elif x1[i1] < x2[i2]:
-
-            if 0 <= i2 - 1:
-                x3.append(x1[i1])
-                y3.append(avg(y1[i1], y2[i2 - 1] + (y2[i2] - y2[i2 - 1]) / (x2[i2] - x2[i2 - 1]) * (x1[i1] - x2[i2 - 1])))
-            
-            i1 += 1
-        
-        elif x2[i2] < x1[i1]:
-            
-            if 0 <= i1 - 1:
-                x3.append(x2[i2])
-                y3.append(avg(y1[i1 - 1] + (y1[i1] - y1[i1 - 1]) / (x1[i1] - x1[i1 - 1]) * (x2[i2] - x1[i1 - 1]), y2[i2]))
-            
-            i2 += 1
-
-        else:
-            assert False
-    
     return (x3, y3)
+    
 
 def point_convert_for_fill_between_get_max(x1: List, y1: List, x2: List, y2: List) -> Tuple [X, Y]:
 
-    x3 = []
+    (x3, y31, y32) = point_convert_for_fill_between_get_both(x1, y1, x2, y2)
     y3 = []
 
-    i1 = 0
-    i2 = 0
+    assert len(y31) == len(y32)
 
-    while i1 < len(x1) and i2 < len(x2):
-
-        if x1[i1] == x2[i2]:
-            x3.append(x1[i1])
-            y3.append(max(y1[i1], y2[i2]))
-            i1 += 1
-            i2 += 1
-
-        elif x1[i1] < x2[i2]:
-
-            if 0 <= i2 - 1:
-                x3.append(x1[i1])
-                y3.append(max(y1[i1], y2[i2 - 1] + (y2[i2] - y2[i2 - 1]) / (x2[i2] - x2[i2 - 1]) * (x1[i1] - x2[i2 - 1])))
-            
-            i1 += 1
-        
-        elif x2[i2] < x1[i1]:
-            
-            if 0 <= i1 - 1:
-                x3.append(x2[i2])
-                y3.append(max(y2[i2], y1[i1 - 1] + (y1[i1] - y1[i1 - 1]) / (x1[i1] - x1[i1 - 1]) * (x2[i2] - x1[i1 - 1])))
-            
-            i2 += 1
-
-        else:
-            assert False
+    for i in range(len(y31)):
+        y3.append(max(y31[i], y32[i]))
     
     return (x3, y3)
 
 
 def point_convert_for_fill_between_get_min(x1: List, y1: List, x2: List, y2: List) -> Tuple [X, Y]:
+    (x3, y31, y32) = point_convert_for_fill_between_get_both(x1, y1, x2, y2)
+    y3 = []
+
+    assert len(y31) == len(y32)
+
+    for i in range(len(y31)):
+        y3.append(min(y31[i], y32[i]))
+
+    return (x3, y3)
+
+def point_convert_for_fill_between_get_both(x1: List, y1: List, x2: List, y2: List) -> Tuple [X, Y1, Y2]:
 
     x3 = []
-    y3 = []
+    y31 = []
+    y32 = []
 
     i1 = 0
     i2 = 0
@@ -103,7 +66,8 @@ def point_convert_for_fill_between_get_min(x1: List, y1: List, x2: List, y2: Lis
 
         if x1[i1] == x2[i2]:
             x3.append(x1[i1])
-            y3.append(min(y1[i1], y2[i2]))
+            y31.append(y1[i1])
+            y32.append(y2[i2])
             i1 += 1
             i2 += 1
 
@@ -111,7 +75,8 @@ def point_convert_for_fill_between_get_min(x1: List, y1: List, x2: List, y2: Lis
 
             if 0 <= i2 - 1:
                 x3.append(x1[i1])
-                y3.append(min(y1[i1], y2[i2 - 1] + (y2[i2] - y2[i2 - 1]) / (x2[i2] - x2[i2 - 1]) * (x1[i1] - x2[i2 - 1])))
+                y31.append(y1[i1])
+                y32.append(y2[i2 - 1] + (y2[i2] - y2[i2 - 1]) / (x2[i2] - x2[i2 - 1]) * (x1[i1] - x2[i2 - 1]))
             
             i1 += 1
         
@@ -119,15 +84,15 @@ def point_convert_for_fill_between_get_min(x1: List, y1: List, x2: List, y2: Lis
             
             if 0 <= i1 - 1:
                 x3.append(x2[i2])
-                y3.append(min(y2[i2], y1[i1 - 1] + (y1[i1] - y1[i1 - 1]) / (x1[i1] - x1[i1 - 1]) * (x2[i2] - x1[i1 - 1])))
+                y31.append(y1[i1 - 1] + (y1[i1] - y1[i1 - 1]) / (x1[i1] - x1[i1 - 1]) * (x2[i2] - x1[i1 - 1]))
+                y32.append(y2[i2])
             
             i2 += 1
 
         else:
             assert False
     
-    return (x3, y3)
-
+    return (x3, y31, y32)
 
 def point_convert_for_fill_between(x1: List, y_min: List, y_max: List, x2: List, y2: List) -> Tuple[X, Y_MIN, Y_MAX]:
 
@@ -153,6 +118,7 @@ class PointConvertForFillBetween:
         self.y_max = []
         self.y_avg = []
         self.weight = 0
+        self.ys = []
 
     def add_plot(self, x: List, y: List):
         
@@ -172,6 +138,68 @@ class PointConvertForFillBetween:
         self.weight += 1
 
 
+class PointConvertForFillBetweenV2:
+
+    def __init__(self):
+        self.xs = []
+        self.ys = []
+        self.x = []
+        self.ys_format = []
+        self.y_min = []
+        self.y_max = []
+        self.y_avg = []
+        self.y_median = []
+        self.y_95confidence_interval_min = []
+        self.y_95confidence_interval_max = []
+
+    def add_plot(self, x: List, y: List):
+        self.xs.append(x)
+        self.ys.append(y)
+    
+    def format_data(self):
+
+        self.x = []
+        self.ys_format = []
+        self.y_min = []
+        self.y_max = []
+        self.y_avg = []
+        self.y_median = []
+
+        x_all = []
+
+        for x in self.xs:
+            x_all += x
+        
+        x_unique = sorted(set(x_all))
+
+        x_range_l = max(x[0]  for x in self.xs)
+        x_range_r = min(x[-1] for x in self.xs)
+
+        for x_v in x_unique:
+            if x_range_l <= x_v and x_v <= x_range_r:
+                self.x.append(x_v)
+
+        for x, y in zip(self.xs, self.ys):
+            (_, y_new, _) = point_convert_for_fill_between_get_both(x, y, self.x, [0] * len(self.x))
+            assert len(y_new) == len(self.x)
+            self.ys_format.append(y_new)
+        
+        last_median = None
+
+        for index in range(len(self.x)):
+            data = [y[index] for y in self.ys_format]
+            self.y_min.append(min(data))
+            self.y_max.append(max(data))
+            self.y_avg.append(sum(data) / len(data))
+            self.y_median.append(median(data))
+
+            if len(self.y_median) > 1:
+                assert self.y_median[-1] >= self.y_median[-2]
+
+            (min_v, max_v) = st.t.interval(alpha=0.95, df=len(data)-1, loc=np.mean(data), scale=st.sem(data))
+            self.y_95confidence_interval_min.append(min_v)
+            self.y_95confidence_interval_max.append(max_v)
+
 if __name__ == '__main__':
     fig = plt.figure(figsize=(9, 7))
     ax: Axes = fig.add_subplot(1, 1, 1)
@@ -188,10 +216,11 @@ if __name__ == '__main__':
     # draw_line([1, 2, 3, 4], [5, 8, 12, 14], 'fuck', ax)
     # draw_line([1.5, 2.5, 3.5, 4.5], [4, 7, 9, 15], 'fuck', ax)
 
-    p = PointConvertForFillBetween()
+    p = PointConvertForFillBetweenV2()
     p.add_plot(x1, y1)
     p.add_plot(x2, y2)
     p.add_plot(x3, y3)
+    p.format_data()
 
     # ax.plot(x1, y1)
     # ax.plot(x2, y2)
